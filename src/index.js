610f58a870ac2241223768e2f1da6c6bbd684d19
Young Jin J <yjung199@gmail.com>
2020-08-19 14:09:44 -0400

    beta release

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const { app, BrowserWindow, nativeTheme, ipcMain } = require('electron');
 const path = require('path');
 const os = require('os');
 
+process.env.NODE_ENV = 'production';
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   // eslint-disable-line global-require
   app.quit();
 }
-
+let debug = false;
 let mainWindow;
 const createWindow = () => {
   // Create the browser window.
@@ -17,19 +19,27 @@ const createWindow = () => {
     webPreferences: {
       nodeIntegration: true,
       nodeIntegrationInWorker: true,
+      devTools: false,
     },
   });
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (debug) mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow, async () => {});
+app.on('ready', createWindow, async () => {
+  // Register a 'Control+Shift+I' shortcut listener.
+
+  globalShortcut.register('Control+Shift+I', () => {
+    //this will get call for Control+Shift+I.
+    return;
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
